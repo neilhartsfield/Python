f16873d3c3f5546911d9b15ab4f3d8ec607211e5
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2022-12-07 15:37:52 -0600

    Update evtConvert.py

--- evtConvert.py
+++ evtConvert.py
@@ -1,17 +1,21 @@
 import sys
-from evtx import EvtxParser
+from evtx import ParseException, ChunkNotFoundError
+from evtx.Evtx import Evtx
 
 
 def convert_evtx_to_txt(evtx_file, txt_file):
     # Open the EVTX file and create a new TXT file
     with open(evtx_file, "rb") as in_file, open(txt_file, "w") as out_file:
         # Parse the EVTX file
-        for record in EvtxParser(in_file):
-            # Extract the XML data from the record
-            xml_string = record.xml()
-            # Write the XML data to the TXT file
-            out_file.write(xml_string)
-            out_file.write("\n")
+        try:
+            for record in Evtx(in_file).records():
+                # Extract the XML data from the record
+                xml_string = record.xml()
+                # Write the XML data to the TXT file
+                out_file.write(xml_string)
+                out_file.write("\n")
+        except (ParseException, ChunkNotFoundError) as error:
+            print("Error parsing EVTX file:", error)
 
 
 if __name__ == "__main__":
