f529b2d6693bff8195c5fbfe24d74c7c0864ab16
Neil Hartsfield <49576227+neilhartsfield@users.noreply.github.com>
2022-12-07 15:31:22 -0600

    Create evtConvert.py

---
+++ evtConvert.py
@@ -0,0 +1,22 @@
+import sys
+import evtx
+
+
+def convert_evtx_to_txt(evtx_file, txt_file):
+    # Open the EVTX file and create a new TXT file
+    with open(evtx_file, "rb") as in_file, open(txt_file, "w") as out_file:
+        # Parse the EVTX file
+        for record in evtx.Evtx(in_file).records():
+            # Extract the XML data from the record
+            xml_string = record.xml()
+            # Write the XML data to the TXT file
+            out_file.write(xml_string)
+            out_file.write("\n")
+
+
+if __name__ == "__main__":
+    # Get the input and output filenames from the command line arguments
+    evtx_file = sys.argv[1]
+    txt_file = sys.argv[2]
+    # Convert the EVTX file to a TXT file
+    convert_evtx_to_txt(evtx_file, txt_file)
